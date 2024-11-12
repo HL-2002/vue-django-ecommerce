d663f9cc324b4a1e479a469a7a77ad682123b8af
HL-2002 <henryslang8231682@gmail.com>
2024-11-12 14:49:11 -0400

    Add scraping script

--- back/API/views.py
+++ back/API/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from . import models
+import json
 
-def say_hello(request: HttpRequest) -> HttpResponse:
+def get_products(request: HttpRequest) -> HttpResponse:
+
     return HttpResponse('Hello Django!')
 
+
+def get_product_by_id(request: HttpRequest) -> HttpResponse:
+
+    return HttpResponse('Hello Django!')
+
+
+def post_product(request: HttpRequest) -> HttpResponse:
+
+    return HttpResponse('Hello Django!')
